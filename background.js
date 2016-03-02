b43b11c7add3aed881140a51b572c9022b9f4ba3
adumin <honoonokitune@live.jp>
2016-03-02 19:49:29 +0900

    固定幅でレイアウトを作成

---
+++ background.js
@@ -0,0 +1,13 @@
+(function(){
+  var posX = 
+  var posY =
+
+  var element = document.createElement("div");
+  element.className = "card";
+  element.width = 100 + "px";
+  element.height = 100 + "px";
+  element.position = absolute;
+  element.top =;
+  element.left =;
+
+})()
